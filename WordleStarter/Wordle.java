860095442583242c262bf81d0422711a2327abf8
BwayBwayA1 <batlas@stu.socsd.org>
2023-06-06 18:27:53 -0400

    Update Wordle.java

--- WordleStarter/Wordle.java
+++ WordleStarter/Wordle.java
@@ -8,93 +8,99 @@
 import edu.willamette.cs1.wordle.WordleDictionary;
 import edu.willamette.cs1.wordle.WordleGWindow;
 import java.awt.Color;
+
 public class Wordle {
+    int guesses;
     String word;
     String checked;
     String alphabet = "abcdefghijklmnopqrstuvwxyz";
-    Color[] rainbow = {Color.RED, Color.ORANGE, Color.YELLOW, Color.CYAN, Color.BLUE, Color.MAGENTA};
+    Color[] rainbow = { Color.RED, Color.ORANGE, Color.YELLOW, Color.CYAN, Color.BLUE, Color.MAGENTA };
+
     public void run() {
         gw = new WordleGWindow();
         gw.addEnterListener((s) -> enterAction(s));
-        //word = WordleDictionary.FIVE_LETTER_WORDS[(int)(Math.random() * WordleDictionary.FIVE_LETTER_WORDS.length)];
-        word = "FLOWN";
+        word = WordleDictionary.FIVE_LETTER_WORDS[(int) (Math.random() *
+                WordleDictionary.FIVE_LETTER_WORDS.length)];
         word = word.toUpperCase();
         // for(int i = 0; i < 5; i++){
-        //     gw.setSquareLetter(0, i, word.substring(i, i+1));
+        // gw.setSquareLetter(0, i, word.substring(i, i+1));
         // }
-        for(int i = 0; i < alphabet.length(); i++){
-            gw.setKeyColor(alphabet.substring(i, i+1), Color.WHITE);
+        for (int i = 0; i < alphabet.length(); i++) {
+            gw.setKeyColor(alphabet.substring(i, i + 1), Color.WHITE);
         }
     }
 
-/*
- * Called when the user hits the RETURN key or clicks the ENTER button,
- * passing in the string of characters on the current row.
- */
+    /*
+     * Called when the user hits the RETURN key or clicks the ENTER button,
+     * passing in the string of characters on the current row.
+     */
 
     public void enterAction(String s) {
-        int index;        
+        if (guesses >= 6) {
+            gw.showMessage("Game over! The hidden word war: " + word);
+        }
+        int index;
         checked = word;
         boolean b = false;
-        for(int i = 0; i < WordleDictionary.FIVE_LETTER_WORDS.length; i++){
-            if((WordleDictionary.FIVE_LETTER_WORDS[i].equalsIgnoreCase(s))){
+        for (int i = 0; i < WordleDictionary.FIVE_LETTER_WORDS.length; i++) {
+            if ((WordleDictionary.FIVE_LETTER_WORDS[i].equalsIgnoreCase(s))) {
                 b = true;
             }
         }
-        if(b){
-            for(int i = 0; i < 5; i++){
-                index = checked.indexOf(s.substring(i, i+1));
-                if(index < 0){
+        if (b) {
+            guesses++;
+            for (int i = 0; i < 5; i++) {
+                index = checked.indexOf(s.substring(i, i + 1));
+                if (index < 0) {
                     gw.setSquareColor(gw.getCurrentRow(), i, WordleGWindow.MISSING_COLOR);
-                    gw.setKeyColor(s.substring(i, i+1), WordleGWindow.MISSING_COLOR);
+                    gw.setKeyColor(s.substring(i, i + 1), WordleGWindow.MISSING_COLOR);
                 }
             }
-            for(int i = 0; i < 5; i++){
-                index = checked.indexOf(s.substring(i, i+1));
-                if(s.substring(i, i+1).equals(checked.substring(i, i+1))){
+            for (int i = 0; i < 5; i++) {
+                index = checked.indexOf(s.substring(i, i + 1));
+                if (s.substring(i, i + 1).equals(checked.substring(i, i + 1))) {
                     gw.setSquareColor(gw.getCurrentRow(), i, WordleGWindow.CORRECT_COLOR);
-                    gw.setKeyColor(s.substring(i, i+1), WordleGWindow.CORRECT_COLOR);
-                    s = s.substring(0, i) + "2" + s.substring(i+1);
-                    checked = checked.substring(0, i) + "1" + checked.substring(i+1);
+                    gw.setKeyColor(s.substring(i, i + 1), WordleGWindow.CORRECT_COLOR);
+                    s = s.substring(0, i) + "2" + s.substring(i + 1);
+                    checked = checked.substring(0, i) + "1" + checked.substring(i + 1);
                 }
             }
-            for(int i = 0; i < 5; i++){
-                index = checked.indexOf(s.substring(i, i+1));
-                if(index >= 0 && s.indexOf(s.substring(i, i+1)) >=0){
+            for (int i = 0; i < 5; i++) {
+                index = checked.indexOf(s.substring(i, i + 1));
+                if (index >= 0 && s.indexOf(s.substring(i, i + 1)) >= 0) {
                     gw.setSquareColor(gw.getCurrentRow(), i, WordleGWindow.PRESENT_COLOR);
-                    if(gw.getKeyColor(s.substring(i, i+1).toUpperCase()) != WordleGWindow.CORRECT_COLOR){
-                        gw.setKeyColor(s.substring(i, i+1), WordleGWindow.PRESENT_COLOR);
+                    if (gw.getKeyColor(s.substring(i, i + 1).toUpperCase()) != WordleGWindow.CORRECT_COLOR) {
+                        gw.setKeyColor(s.substring(i, i + 1), WordleGWindow.PRESENT_COLOR);
                     }
-                    s = s.substring(0, i) + "2" + s.substring(i+1);
-                    checked = (checked.substring(0, index) + "1" + checked.substring(index + 1));                   
+                    s = s.substring(0, i) + "2" + s.substring(i + 1);
+                    checked = (checked.substring(0, index) + "1" + checked.substring(index + 1));
                 }
-            }          
-                if(checked.equals("11111")){
-                    gw.showMessage("CORRECT!");
-                    for(int r = 0; r <= gw.getCurrentRow(); r++){
-                        for(int c = 0; c <= WordleGWindow.N_COLS; c++){
-                            if(gw.getSquareColor(r, c).equals(WordleGWindow.CORRECT_COLOR) || gw.getSquareColor(r, c).equals(WordleGWindow.PRESENT_COLOR)){
-                                gw.setSquareColor(r, c, rainbow[(int)(Math.random() * rainbow.length)]);
-                                gw.setKeyColor(gw.getSquareLetter(r, c),  rainbow[(int)(Math.random() * rainbow.length)]);
-                            }
+            }
+            if (checked.equals("11111")) {
+                gw.showMessage("CORRECT!");
+                for (int r = 0; r <= gw.getCurrentRow(); r++) {
+                    for (int c = 0; c < WordleGWindow.N_COLS; c++) {
+                        if (gw.getSquareColor(r, c).equals(WordleGWindow.CORRECT_COLOR)
+                                || gw.getSquareColor(r, c).equals(WordleGWindow.PRESENT_COLOR)) {
+                            gw.setSquareColor(r, c, rainbow[(int) (Math.random() * rainbow.length)]);
+                            gw.setKeyColor(gw.getSquareLetter(r, c), rainbow[(int) (Math.random() * rainbow.length)]);
                         }
                     }
-                    gw.setCurrentRow(8);
                 }
-                gw.setCurrentRow(gw.getCurrentRow()+1);
+                gw.setCurrentRow(8);
             }
-        else{
+            gw.setCurrentRow(gw.getCurrentRow() + 1);
+        } else {
             gw.showMessage("Not in word list");
         }
     }
-    
 
-/* Startup code */
+    /* Startup code */
     public static void main(String[] args) {
         new Wordle().run();
     }
 
-/* Private instance variables */
+    /* Private instance variables */
 
     private WordleGWindow gw;
 
